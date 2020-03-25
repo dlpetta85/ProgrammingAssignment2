dc043e5b5567813a3b0fafb06e6fa9da9a318111
dlpetta85 <de.leon.petta@hotmail.com>
2020-03-24 23:31:00 -0300

    week3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##Creation of the matrix. This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(mat = matrix()) {
+  ## Setting the matrix
+  alt <- NULL
+  set <- function(y) {
+    mat <<- y
+    alt <<- NULL
+  }
+  ##Getting the matrix
+  get <- function() mat
+  setalternado <- function(inverse) alt <<- inverse
+  getalternado <- function() alt
+  list(set = set,
+       get = get,
+       setalternado = setalternado,
+       getalternado = getalternado)
 }
 
+##unused space
+##unused space
+##unused space
+##unused space
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+cacheSolve <- function(mat, ...) {
+  ##returning the inverse matrix 
+  alt <- mat$getalternado()
+  if(!is.null(alt)) {
+    return(alt)
+  }
+  ##Getting the matrix
+  dados <- mat$get()
+  ##calculating the inverse
+  alt <- solve(dados)
+  ##Setting the inverse
+  mat$setalternado(alt)
+  alt
 }
